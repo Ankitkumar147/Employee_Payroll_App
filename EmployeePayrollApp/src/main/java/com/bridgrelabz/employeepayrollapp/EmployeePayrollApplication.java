bde5025c03a73331a5333e356343e3ab169a674f
Ankitkumar147 <aankitchouhan3@gmail.com>
2022-09-01 23:06:29 +0530

    Lombok Library for logging.

---
+++ EmployeePayrollApp/src/main/java/com/bridgrelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -0,0 +1,18 @@
+package com.bridgrelabz.employeepayrollapp;
+
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * Using Lombok Library for Logging -----> Slf4j
+ */
+@SpringBootApplication
+@Slf4j
+public class EmployeePayrollApplication {
+
+    public static void main(String[] args) {
+       SpringApplication.run(EmployeePayrollApplication.class, args);
+        log.info("Employee Payroll App Started.");
+    }
+}
