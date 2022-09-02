6115ba743425d8bde397414026e19389072b5162
Ankitkumar147 <aankitchouhan3@gmail.com>
2022-09-02 09:55:46 +0530

    database environment variable set for username and password.

--- EmployeePayrollApp/src/main/java/com/bridgrelabz/employeepayrollapp/EmployeePayrollApplication.java
+++ EmployeePayrollApp/src/main/java/com/bridgrelabz/employeepayrollapp/EmployeePayrollApplication.java
@@ -1,12 +1,22 @@
 package com.bridgrelabz.employeepayrollapp;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
 
+/**
+ * Using Lombok Library for Logging -----> Slf4j
+ * Created Application.Properties for dev and production.
+ *
+ */
 @SpringBootApplication
+@Slf4j
 public class EmployeePayrollApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(EmployeePayrollApplication.class, args);
+       ApplicationContext context = SpringApplication.run(EmployeePayrollApplication.class, args);
+        log.info("Employee Payroll App Started in {} Environment.",context.getEnvironment().getProperty("environment"));
+        System.out.println("Welcome to the Employee Payroll Application.");
     }
 }
